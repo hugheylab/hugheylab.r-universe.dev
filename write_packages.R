14542d008877fd2dd77608e46ab8dd95f5c5fb56
Jake Hughey <jakejhughey@gmail.com>
2021-11-18 12:27:43 -0600

    removed metapredict, tweaked syntax.

--- write_packages.R
+++ write_packages.R
@@ -1,9 +1,18 @@
 library('data.table')
 library('jsonlite')
 
-d = data.table(
-  package = c('metapredict', 'zeitzeiger', 'deltaccd', 'tipa', 'limorhyde',
-              'simphony', 'spectr', 'pmparser', 'limorhyde2', 'seeker'))
+d = data.table(package = c(
+  # 'metapredict', # exclude due to vignettes
+  'zeitzeiger',
+  'deltaccd',
+  'tipa',
+  'limorhyde',
+  'simphony',
+  'spectr',
+  'pmparser',
+  'limorhyde2',
+  'seeker'))
+
 d[, url := paste0('https://github.com/hugheylab/', package)]
 
 write_json(d, 'packages.json', pretty = TRUE)
